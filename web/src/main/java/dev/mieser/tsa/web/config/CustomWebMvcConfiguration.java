d62d84eb5e5aba40146647a7e25104ab95c4c8d7
Daniel <danielmeier50@gmail.com>
2022-02-18 20:47:31 +0100

    Apply Spotless

--- web/src/main/java/dev/mieser/tsa/web/config/CustomWebMvcConfiguration.java
+++ web/src/main/java/dev/mieser/tsa/web/config/CustomWebMvcConfiguration.java
@@ -9,10 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import dev.mieser.tsa.web.formatter.Base64Formatter;
 import dev.mieser.tsa.web.formatter.HexFormatter;
